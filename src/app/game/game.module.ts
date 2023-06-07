0a1c44bbad1526cf5a4e6dd07ab0c2d45628fe83
Patrik Obrist <padx@gmx.net>
2023-06-07 12:56:11 +0200

    add ranking component

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameShellComponent } from './game-shell/game-shell.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 
 
 @NgModule({
   declarations: [
-    GameShellComponent
+    GameShellComponent,
+    RankingComponent
   ],
   imports: [
     CommonModule
